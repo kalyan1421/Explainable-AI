e6ff386f5d946530bcc307ad8da7b2a1b7f82c00
kalyan1421 <kalyan91333@gmail.com>
2025-12-29 18:05:49 +0530

    Fix Android minSdkVersion to 23 for cloud_firestore compatibility

--- explainable_ai/android/app/build.gradle.kts
+++ explainable_ai/android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.example.explainable_ai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23  // Required for cloud_firestore
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
